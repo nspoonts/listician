ba761f74223399cbaa2e2340a061a8f106367722
Nathan Spoonts <nspoonts@gmail.com>
2012-07-02 13:50:12 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listician::Application.config.secret_token = '5e676eceab506d1680550b6a6a6df2c9ca6de3c2179d3c4bbf4167b3d5270424e17dd62c0779ec0fa458a3d0c264d3d6008879f75ba994200a7126c87005c888'
