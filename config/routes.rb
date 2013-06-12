6cd7d0bde91ea704d16e7924c31e763e919d70b1
Nathan Spoonts <nspoonts@gmail.com>
2013-06-12 11:17:43 -0500

    Added email signup page

--- config/routes.rb
+++ config/routes.rb
@@ -63,7 +63,7 @@ Listician::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'email#signup'
+  root :to => 'email#new'
 
   # See how all your routes lay out with "rake routes"
 
